24dcc80c0f6a199855571df4f5e7607445432c4a
Gabriel Cioci <ciocigabi@gmail.com>
2020-03-03 15:54:56 +0200

    Updates

---
+++ precache-manifest.30455cf9c6129f9208ad1e79a9a7eed8.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d33865254b100a27c5c56d622207f98a",
+    "url": "/programming-quotes/index.html"
+  },
+  {
+    "revision": "159433382d3d031b626f",
+    "url": "/programming-quotes/static/css/main.9c468699.chunk.css"
+  },
+  {
+    "revision": "98c7751aa35023f5259f",
+    "url": "/programming-quotes/static/js/2.5cd53c91.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/programming-quotes/static/js/2.5cd53c91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "159433382d3d031b626f",
+    "url": "/programming-quotes/static/js/main.bad1acd6.chunk.js"
+  },
+  {
+    "revision": "5a00799071ee308b511c",
+    "url": "/programming-quotes/static/js/runtime-main.fc72b0df.js"
+  },
+  {
+    "revision": "a0f3e2f9a1724b929c89649ed115c208",
+    "url": "/programming-quotes/static/media/art.a0f3e2f9.svg"
+  },
+  {
+    "revision": "473c98fe9850e4a350bee6a71bc483e7",
+    "url": "/programming-quotes/static/media/imagination.473c98fe.svg"
+  },
+  {
+    "revision": "8b09de3dd0c5cfc55c4ba656bf0db4bb",
+    "url": "/programming-quotes/static/media/inspire.8b09de3d.svg"
+  },
+  {
+    "revision": "88556646f36a87deb50fa62baaa19bf6",
+    "url": "/programming-quotes/static/media/mobiledev.88556646.svg"
+  },
+  {
+    "revision": "06b9308079aabea3403c98f638d86271",
+    "url": "/programming-quotes/static/media/omega.06b93080.svg"
+  },
+  {
+    "revision": "2c3054c1419cb42581accf92387d8412",
+    "url": "/programming-quotes/static/media/opinions.2c3054c1.svg"
+  },
+  {
+    "revision": "41f8b740a193c87883c9db9731005102",
+    "url": "/programming-quotes/static/media/people.41f8b740.svg"
+  },
+  {
+    "revision": "ab129dd2f0a3e621dd6a6d82eca69ee2",
+    "url": "/programming-quotes/static/media/road.ab129dd2.svg"
+  },
+  {
+    "revision": "97968f99ed853106106cb68d4f074cce",
+    "url": "/programming-quotes/static/media/socials.97968f99.svg"
+  },
+  {
+    "revision": "a96d368a8c56c89785602902d5f6811d",
+    "url": "/programming-quotes/static/media/tea.a96d368a.svg"
+  },
+  {
+    "revision": "6e2942332f31ea52d48d5a35f56eca5c",
+    "url": "/programming-quotes/static/media/time.6e294233.svg"
+  }
+]);
